6650b4bd7a893b19146efc4565e3f1e12040fe6f
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-03-18 15:30:15 +0530

    solution

---
+++ 3.c
@@ -0,0 +1,54 @@
+/*
+	Q3. Write a function to implement four function calculator. Function would take operands and
+	operator as arguments and returns result.
+*/
+
+
+#include<stdio.h>
+
+int calculator(int, char, int);
+
+int main()
+{
+	int num1, num2;
+	char opr;
+	printf("Enter the 1st number: ");
+	scanf("%d",&num1);
+	printf("\nEnter the operand: ");
+	scanf("%*c%c",&opr);
+	printf("\nEnter the 2nd number: ");
+	scanf("%d",&num2);
+
+	int res=calculator(num1, opr, num2);
+	printf("The Result of above expression is %d\n",res);
+
+	return 0;
+}
+
+int calculator(int num1, char opr, int num2)
+{
+	int result;
+
+	switch(opr)
+	{
+		case '+':
+					result = num1 + num2;
+					break;
+		case '-':
+					result = num1 - num2;
+					break;
+		case '*':
+					result = num1 * num2;
+					break;
+		case '/':
+					if(num2 != 0)
+						result = num1 / num2;
+					else
+						printf("Denominator is zero error.\n");
+					break;
+		default :
+					printf("Invalid operation\n");
+	}
+
+	return result;
+}
